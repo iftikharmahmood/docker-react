3b7d577c9a570d8562e562fb20985372d92132b1
iftikharmahmood <47450543+iftikharmahmood@users.noreply.github.com>
2019-02-11 17:54:51 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
         #  Edit <code>src/App.js</code> and save to reload.
-            Hi there!
+            Changed on feature2 branch
           </p>
           <a
             className="App-link"
